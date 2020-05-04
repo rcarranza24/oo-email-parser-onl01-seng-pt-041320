86fae864ccfe2b15004588bc9ecade3321608d1f
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-04 23:17:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,7 +2,14 @@ class EmailAddressParser
   
   attr_accessor :email 
   def initialize(emails)
-    @email = emails 
+    @emails = emails 
   end 
   
-  
+
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
+end
